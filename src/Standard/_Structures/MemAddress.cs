9854c7839045a75507803e0023dcccb98b005b8b
mayli4 <sydsleeper@gmail.com>
2025-11-04 10:14:38 -0500

    feat: mem address

---
+++ src/Standard/_Structures/MemAddress.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Standard;
+
+public readonly unsafe struct MemAddress(void* voidPtr) : IDisposable {
+    public static readonly int Size = IntPtr.Size;
+
+    private readonly byte* _ptr = (byte*)voidPtr;
+    
+    public readonly ref byte this[int index] {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => ref _ptr[(uint)index];
+    }
+
+    public void Dispose() => Marshal.FreeHGlobal(new IntPtr(_ptr));
+}
