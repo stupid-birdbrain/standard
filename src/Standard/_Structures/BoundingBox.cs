e4dae64056817212c13c19098828ffb34cb42305
mayli4 <sydsleeper@gmail.com>
2025-11-29 12:16:46 -0500

    feat: bounding box

---
+++ src/Standard/_Structures/BoundingBox.cs
@@ -0,0 +1,86 @@
+﻿using System.Numerics;
+
+namespace Standard;
+
+/// <summary>
+///     An axis-aligned bounding box.
+/// </summary>
+public struct BoundingBox(Vector3 min, Vector3 max) {
+    public Vector3 Min = min;
+    public Vector3 Max = max;
+
+    /// <summary>
+    ///     Gets the 8 corners of the bounding box.
+    /// </summary>
+    public ReadOnlySpan<Vector3> GetCorners() {
+        Span<Vector3> corners = new Vector3[8];
+        corners[0] = new Vector3(Min.X, Min.Y, Min.Z);
+        corners[1] = new Vector3(Max.X, Min.Y, Min.Z);
+        corners[2] = new Vector3(Max.X, Max.Y, Min.Z);
+        corners[3] = new Vector3(Min.X, Max.Y, Min.Z);
+        corners[4] = new Vector3(Min.X, Min.Y, Max.Z);
+        corners[5] = new Vector3(Max.X, Min.Y, Max.Z);
+        corners[6] = new Vector3(Max.X, Max.Y, Max.Z);
+        corners[7] = new Vector3(Min.X, Max.Y, Max.Z);
+        return corners;
+    }
+
+    /// <summary>
+    ///     Checks if the bounding box intersects or is inside a plane.
+    /// </summary>
+    public bool Intersects(in Plane plane) {
+        Vector3 pVertex;
+        Vector3 nVertex;
+
+        // x
+        if (plane.Normal.X >= 0) {
+            pVertex.X = Max.X;
+            nVertex.X = Min.X;
+        } else {
+            pVertex.X = Min.X;
+            nVertex.X = Max.X;
+        }
+
+        // y
+        if (plane.Normal.Y >= 0) {
+            pVertex.Y = Max.Y;
+            nVertex.Y = Min.Y;
+        } else {
+            pVertex.Y = Min.Y;
+            nVertex.Y = Max.Y;
+        }
+
+        // z
+        if (plane.Normal.Z >= 0) {
+            pVertex.Z = Max.Z;
+            nVertex.Z = Min.Z;
+        } else {
+            pVertex.Z = Min.Z;
+            nVertex.Z = Max.Z;
+        }
+
+        if (Plane.DotCoordinate(plane, pVertex) < 0) {
+            return false;
+        }
+
+        // if n vertex is infront of the plane, the entire box is infront of the plane
+        return true;
+    }
+    
+    /// <summary>
+    ///     Checks if this bounding box intersects another bounding box.
+    /// </summary>
+    public bool Intersects(in BoundingBox other) {
+        if (this.Max.X < other.Min.X || this.Min.X > other.Max.X) {
+            return false;
+        }
+        if (this.Max.Y < other.Min.Y || this.Min.Y > other.Max.Y) {
+            return false;
+        }
+        if (this.Max.Z < other.Min.Z || this.Min.Z > other.Max.Z) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/Tests/BoundingBoxTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Standard;
+using System.Numerics;
+
+namespace Tests;
+
+[TestFixture]
+public sealed class BoundingBoxTests {
+    [Test]
+    public void Intersects_BoxContainedWithinAnother_ReturnsTrue() {
+        var outerBox = new BoundingBox(new Vector3(-5, -5, -5), new Vector3(5, 5, 5));
+        var innerBox = new BoundingBox(new Vector3(-1, -1, -1), new Vector3(1, 1, 1));
+
+        Assert.That(outerBox.Intersects(innerBox), Is.True);
+        Assert.That(innerBox.Intersects(outerBox), Is.True);
+    }
+    
+        [Test]
+    public void GetCorners_ReturnsEightCorners() {
+        var box = new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1));
+        var corners = box.GetCorners();
+
+        Assert.That(corners.Length, Is.EqualTo(8));
+    }
+
+    [Test]
+    public void GetCorners_ReturnsCorrectCornerValues() {
+        var min = new Vector3(0, 0, 0);
+        var max = new Vector3(1, 1, 1);
+        var box = new BoundingBox(min, max);
+        var actualCorners = box.GetCorners();
+
+        var expectedCorners = new[] {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0), 
+            new Vector3(1, 1, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(0, 1, 1)
+        };
+
+        foreach (var expectedCorner in expectedCorners) {
+            Assert.That(actualCorners.ToArray(), Does.Contain(expectedCorner), $"expected corner {expectedCorner} not found!");
+        }
+
+        Assert.That(actualCorners[0], Is.EqualTo(new Vector3(min.X, min.Y, min.Z)));
+        Assert.That(actualCorners[1], Is.EqualTo(new Vector3(max.X, min.Y, min.Z)));
+        Assert.That(actualCorners[2], Is.EqualTo(new Vector3(max.X, max.Y, min.Z)));
+        Assert.That(actualCorners[3], Is.EqualTo(new Vector3(min.X, max.Y, min.Z)));
+        Assert.That(actualCorners[4], Is.EqualTo(new Vector3(min.X, min.Y, max.Z)));
+        Assert.That(actualCorners[5], Is.EqualTo(new Vector3(max.X, min.Y, max.Z)));
+        Assert.That(actualCorners[6], Is.EqualTo(new Vector3(max.X, max.Y, max.Z)));
+        Assert.That(actualCorners[7], Is.EqualTo(new Vector3(min.X, max.Y, max.Z)));
+    }
+}
